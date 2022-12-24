cbe9648b4ae4cb75562b893a9ca91bec36840e27
toms0910 <psp1070@gmail.com>
2022-12-24 18:45:39 +0900

    setup

---
+++ src/main/java/com/github/shiro8613/growsugarcane/GrowSugarCane.java
@@ -0,0 +1,17 @@
+package com.github.shiro8613.growsugarcane;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GrowSugarCane extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
