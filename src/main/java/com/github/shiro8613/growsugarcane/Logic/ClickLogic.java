c1db4576be957b4c43c654ba21ce4d65b0e6a4ab
toms0910 <psp1070@gmail.com>
2022-12-27 22:45:18 +0900

    クリックロジッククラス作成

---
+++ src/main/java/com/github/shiro8613/growsugarcane/Logic/ClickLogic.java
@@ -0,0 +1,11 @@
+package com.github.shiro8613.growsugarcane.Logic;
+
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class ClickLogic {
+
+    public void Action(@NotNull Player player) {
+
+    }
+}
